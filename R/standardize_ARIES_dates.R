6f3efcb3087eb8b7e024bd02d989a026ae4736c9
Dan Riggins <daniel.riggins@protonmail.com>
2021-02-11 20:15:25 -0500

    Documenting standardize_ARIES_dates()

--- R/standardize_ARIES_dates.R
+++ R/standardize_ARIES_dates.R
@@ -1,3 +1,11 @@
+#' Ensure all dates in an ARIES dataframe follow the same convention of year-month-day
+#'
+#' @param df A tibble of data imported using read_ARIES_csv()
+#'
+#' @return A tibble with standardized dates
+#' @export
+#'
+#' @examples
 standardize_ARIES_dates <- function(df) {
     df$COLLDTE <- df$COLLDTE %>%
     lubridate::ymd() %>%
